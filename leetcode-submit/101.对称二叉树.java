b44ef59581bf820d0ac420fa9c137a363e024273
stevenyuysy <stevenyuysy@gmail.com>
2020-07-04 08:55:20 +0800

    isSymmetric tree

--- leetcode-submit/101.对称二叉树.java
+++ leetcode-submit/101.对称二叉树.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 
 import javax.swing.tree.TreeNode;
@@ -20,20 +22,61 @@ class Solution {
             return true;
         }
         queue.add(root);
-        int level = 1;
         int count = 0;
+        int nextCount = 1;
+        ArrayList<Integer> compareList = new ArrayList<Integer>();
         while (!queue.isEmpty()) {
             TreeNode node = queue.remove();
-            // visit node here
-
-            if (node.left != null) {
-                queue.add(node.left);
+            if (count < nextCount) {
+                compareList.add(node != null ? node.val : null);
+                count++;
+            } else {
+                if (compareList.size() != nextCount) {
+                    return false;
+                }
+                boolean isSymmetricList = checkIsSymmetricList(compareList);
+                if (isSymmetricList == false) {
+                    return false;
+                }
+                compareList.removeAll(Collections.singleton(null));
+                nextCount = compareList.size() * 2;
+                compareList.clear();
+                compareList.add(node != null ? node.val : null);
+                count = 1;
             }
-            if (node.right != null) {
+            if (node != null) {
+                queue.add(node.left);
                 queue.add(node.right);
             }
         }
+        if (!compareList.isEmpty()) {
+            if (compareList.size() != nextCount) {
+                return false;
+            }
+            boolean isSymmetricList = checkIsSymmetricList(compareList);
+            if (isSymmetricList == false) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    private boolean checkIsSymmetricList(ArrayList<Integer> list) {
+        boolean isSymmetricList = true;
+        int length = list.size();
+        for (int i = 0; i < length / 2; i++) {
+            int symmetricIndex = length - 1 - i;
+            if (list.get(i) != null) {
+                if (!list.get(i).equals(list.get(symmetricIndex))) {
+                    isSymmetricList = false;
+                    break;
+                }
+            } else if (list.get(i) != list.get(symmetricIndex)) {
+                isSymmetricList = false;
+                break;
+            }
+        }
+        return isSymmetricList;
     }
 }
 // @lc code=end
