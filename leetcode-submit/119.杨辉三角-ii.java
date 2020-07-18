4252414bb5fd61192ba32bbcd37a5331fe16c173
stevenyuysy <stevenyuysy@gmail.com>
2020-07-18 09:49:46 +0800

    get row

--- leetcode-submit/119.杨辉三角-ii.java
+++ leetcode-submit/119.杨辉三角-ii.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /*
  * @lc app=leetcode.cn id=119 lang=java
  *
@@ -7,17 +9,17 @@
 // @lc code=start
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        if (rowIndex == 0) {
-            return list;
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            intList.add(0, 1);
+            int length = intList.size();
+            for (int j = 1; j < length - 1; j++) {
+                Integer diff = intList.get(j) + intList.get(j + 1);
+                intList.set(j, diff);
+            }
         }
-        List<Integer> lastList = getRow(rowIndex - 1);
-        for (int i = 1; i < lastList.size(); i++) {
-            list.add(lastList.get(i - 1) + lastList.get(i));
-        }
-        list.add(1);
-        return list;
+        return intList;
     }
 }
 // @lc code=end
