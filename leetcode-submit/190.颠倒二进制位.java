86a3c322e5c18ef9014186220542d67eb4924894
stevenyuysy <stevenyuysy@gmail.com>
2020-07-26 18:09:28 +0800

    reverseBits

--- leetcode-submit/190.颠倒二进制位.java
+++ leetcode-submit/190.颠倒二进制位.java
@@ -8,40 +8,14 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        String bitN = tenToBit(n);
-        long r = 0;
-        for (int i = 0; i < bitN.length(); i++) {
-            int currentInt = Character.getNumericValue(bitN.charAt(i));
-            long tenInt = currentInt * (long) Math.pow(2, i);
-            r += tenInt;
+        int res = 0;
+        int power = 31;
+        while (power >= 0) {
+            res += (n & 1) << power;
+            power--;
+            n >>= 1;
         }
-        return (int) r;
-    }
-
-    private String tenToBit(int n) {
-        String r = "";
-        boolean flag = true;
-        if (n < 0) {
-            n = (-n) - 1;
-            flag = false;
-        }
-        while (n != 0) {
-            int i = (n % 2);
-            r = String.valueOf(i) + r;
-            n /= 2;
-        }
-        while (r.length() < 32) {
-            r = "0" + r;
-        }
-        if (!flag) {
-            String newR = "";
-            for (int i = 0; i < r.length(); i++) {
-                newR += r.charAt(i) == '1' ? '0' : '1';
-            }
-            System.out.println("newR " + newR);
-            return newR;
-        }
-        return r;
+        return res;
     }
 
 }
