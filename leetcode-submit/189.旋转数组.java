29899f9d4e54d310243f54cff26962fa92da63f0
stevenyuysy <stevenyuysy@gmail.com>
2020-07-26 11:07:49 +0800

    rotate

---
+++ leetcode-submit/189.旋转数组.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=java
+ *
+ * [189] 旋转数组
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int rK = k % nums.length;
+        for (int i = 0; i < rK; i++) {
+            moveOne(nums);
+        }
+    }
+
+    private void moveOne(int[] nums) {
+        int temp = nums[nums.length - 1];
+        for (int i = nums.length - 1; i > 0; i--) {
+            nums[i] = nums[i - 1];
+        }
+        nums[0] = temp;
+    }
+}
+// @lc code=end
